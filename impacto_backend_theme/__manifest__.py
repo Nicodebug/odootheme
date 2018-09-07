14b3810850fd2ca8c05b90e62b7be938cde3f8f7
Nicodebug <contacto@allweb.co>
2018-09-07 17:32:10 -0500

    Initial commit

---
+++ impacto_backend_theme/__manifest__.py
@@ -0,0 +1,37 @@
+
+{
+    "name": "Impacto Backend theme",
+    "summary": "Odoo 11.0 community backend theme",
+    "version": "11.0.1.0.23",
+    "category": "Themes/Backend",
+    "author": 'Adventumweb',
+    "description": """
+================================================================================
+
+Odoo admin responsive backend theme 
+
+================================================================================
+""",
+    'depends': ['base', 'web'],
+    'data': [
+        'views/res_company_view.xml',
+        'views/assets.xml',
+        'views/web.xml',
+        'views/users.xml',
+        'views/sidebar.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'application': True,
+    'auto_install': False,
+    'installable': True,
+    'web_preload': True,
+    'currency': 'EUR',
+	'live_test_url':'http://167.99.84.41:8069',
+	'images': [
+      'static/description/impacto.jpg',
+ 	  'static/description/impactobackend_screenshot.jpg'
+    ],
+}
+
